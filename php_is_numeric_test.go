0532e385313a079ca469f12f02bab78383242f72
Charles <charles@reptile.ca>
2013-05-09 15:05:05 -0700

    added more tests

--- php_is_numeric_test.go
+++ php_is_numeric_test.go
@@ -404,6 +404,230 @@ func TestIsNumericInt64(t *testing.T) {
 
 
 
+func TestIsNumericFloat32(t *testing.T) {
+
+        var x float32
+
+
+	x = 0.0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float32 [%v]", x)
+	}
+
+	x = 1.0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float32 [%v]", x)
+	}
+
+	x = -1.0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float32 [%v]", x)
+	}
+
+	x = 12.34
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float32 [%v]", x)
+	}
+
+	x = -12.34
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float32 [%v]", x)
+	}
+}
+
+func TestIsNumericFloat64(t *testing.T) {
+
+        var x float64
+
+
+	x = 0.0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float64 [%v]", x)
+	}
+
+	x = 1.0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float64 [%v]", x)
+	}
+
+	x = -1.0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float64 [%v]", x)
+	}
+
+	x = 12.34
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float64 [%v]", x)
+	}
+
+	x = -12.34
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for float64 [%v]", x)
+	}
+}
+
+
+
+func TestIsNumericComplex64(t *testing.T) {
+
+        var x complex64
+
+
+	x = 0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for complex64 [%v]", x)
+	}
+
+	x = 1i
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for complex64 [%v]", x)
+	}
+
+	x = -1i
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for complex64 [%v]", x)
+	}
+
+	x = 12.34i
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for complex64 [%v]", x)
+	}
+
+	x = -12.34i
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for complex64 [%v]", x)
+	}
+
+	x = 12.34 + 56.78i
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for complex64 [%v]", x)
+	}
+
+	x = -12.34 + -56.78i
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for complex64 [%v]", x)
+	}
+}
+
+
+
+func TestIsNumericUint(t *testing.T) {
+
+        var x uint
+
+
+
+	x = 0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for uint [%v]", x)
+	}
+
+	x = 5
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for uint [%v]", x)
+	}
+
+	x = 1
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for uint [%v]", x)
+	}
+
+	x = 123
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for uint [%v]", x)
+	}
+
+	x = 255
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for uint [%v]", x)
+	}
+}
+
+func TestIsNumericInt(t *testing.T) {
+
+        var x int
+
+
+
+	x = 0
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+
+	x = 5
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+
+	x = -5
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+
+	x = 1
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+
+	x = -1
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+
+	x = 123
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+
+	x = -123
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+
+	x = 127
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+
+	x = -127
+
+	if !IsNumeric(x)  {
+	        t.Errorf("IsNumeric() should have returned true for int [%v]", x)
+	}
+}
+
+
+
 func TestIsNumericString(t *testing.T) {
 
         var x string
