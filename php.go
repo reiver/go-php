101f8ba441e57940a37704f7241981bf5ed9d9bb
Charles <charles@reptile.ca>
2013-05-09 18:06:59 -0700

    added php.Serialize()

--- php.go
+++ php.go
@@ -2,6 +2,12 @@ package php
 
 
 
+import (
+	"strconv"
+)
+
+
+
 func IsNumeric(x interface{}) bool {
 
 	// Initialize
@@ -121,3 +127,62 @@ func isStringNumeric(x string) bool {
 	return result
 }
 
+
+
+func Serialize(x interface{}) string {
+
+	// Initialize.
+		serialized := ""
+
+		switch x.(type) {
+			default:
+				serialized = ""
+
+			case uint8:
+				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint8)), 10) +";"
+			case uint16:
+				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint16)), 10) +";"
+			case uint32:
+				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint32)), 10) +";"
+			case uint64:
+				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint64)), 10) +";"
+
+			case int8:
+				serialized = "i:"+ strconv.FormatInt(int64(x.(int8)), 10) +";"
+			case int16:
+				serialized = "i:"+ strconv.FormatInt(int64(x.(int16)), 10) +";"
+			case int32:
+				serialized = "i:"+ strconv.FormatInt(int64(x.(int32)), 10) +";"
+			case int64:
+				serialized = "i:"+ strconv.FormatInt(int64(x.(int64)), 10) +";"
+
+			case float32:
+				serialized = ""
+			case float64:
+				serialized = ""
+
+			case complex64:
+				serialized = ""
+			case complex128:
+				serialized = ""
+
+			//case byte:
+			//	serialized = "i:"+ strconv.FormatInt(x.(byte), 10) +";"
+			//case rune:
+			//	serialized = "i:"+ strconv.FormatInt(x.(rune), 10) +";"
+
+			case uint:
+				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint)), 10) +";"
+			case int:
+				serialized = "i:"+ strconv.FormatInt(int64(x.(int)), 10) +";"
+
+			case string:
+				serialized = "s:"+ strconv.FormatInt(int64(len(x.(string))), 10) +":\""+ x.(string) +"\";"
+
+//@TODO: Add support for maps, slices and arrays
+		} // switch
+
+
+	// Return.
+		return serialized	
+}
