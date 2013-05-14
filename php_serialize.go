94e2b650b5e3b18089321c816c0ff5333e1128ae
Charles <charles@reptile.ca>
2013-05-14 16:06:50 -0700

    made it a bit more efficient by using bytes.Buffer (for concatenation) instead of string

--- php_serialize.go
+++ php_serialize.go
@@ -3,6 +3,7 @@ package php
 
 
 import (
+	"bytes"
 	"strconv"
 )
 
@@ -11,69 +12,109 @@ import (
 func Serialize(x interface{}) string {
 
 	// Initialize.
-		serialized := ""
+		var serialized bytes.Buffer
 
 		switch x.(type) {
 			case uint8:
-				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint8)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatUint(uint64(x.(uint8)), 10)   )
+				serialized.WriteString(";")
 			case uint16:
-				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint16)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatUint(uint64(x.(uint16)), 10)   )
+				serialized.WriteString(";")
 			case uint32:
-				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint32)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatUint(uint64(x.(uint32)), 10)   )
+				serialized.WriteString(";")
 			case uint64:
-				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint64)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatUint(uint64(x.(uint64)), 10)   )
+				serialized.WriteString(";")
 
 			case int8:
-				serialized = "i:"+ strconv.FormatInt(int64(x.(int8)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatInt(int64(x.(int8)), 10)   )
+				serialized.WriteString(";")
 			case int16:
-				serialized = "i:"+ strconv.FormatInt(int64(x.(int16)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatInt(int64(x.(int16)), 10)   )
+				serialized.WriteString(";")
 			case int32:
-				serialized = "i:"+ strconv.FormatInt(int64(x.(int32)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatInt(int64(x.(int32)), 10)   )
+				serialized.WriteString(";")
 			case int64:
-				serialized = "i:"+ strconv.FormatInt(int64(x.(int64)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatInt(int64(x.(int64)), 10)   )
+				serialized.WriteString(";")
 
 			case float32:
-				serialized = serializeFloat32(x.(float32))
+				serialized.WriteString(   serializeFloat32(x.(float32))   )
 			case float64:
-				serialized = serializeFloat64(x.(float64))
+				serialized.WriteString(   serializeFloat64(x.(float64))   )
 
 			case complex64:
-				serialized = "a:2:{s:4:\"real\";"+ serializeFloat32(real(x.(complex64))) +"s:4:\"imag\";"+ serializeFloat32(imag(x.(complex64))) +"}"
+				serialized.WriteString("a:2:{s:4:\"real\";")
+				serialized.WriteString(   serializeFloat32(real(x.(complex64)))   )
+				serialized.WriteString("s:4:\"imag\";")
+				serialized.WriteString(   serializeFloat32(imag(x.(complex64)))   )
+				serialized.WriteString("}")
 			case complex128:
-				serialized = "a:2:{s:4:\"real\";"+ serializeFloat64(real(x.(complex128))) +"s:4:\"imag\";"+ serializeFloat64(imag(x.(complex128))) +"}"
+				serialized.WriteString("a:2:{s:4:\"real\";")
+				serialized.WriteString(   serializeFloat64(real(x.(complex128)))   )
+				serialized.WriteString("s:4:\"imag\";")
+				serialized.WriteString(   serializeFloat64(imag(x.(complex128)))   )
+				serialized.WriteString("}")
 
 			//case byte:
-			//	serialized = "i:"+ strconv.FormatInt(int64(x.(byte)), 10) +";"
+			//	serialized.WriteString("i:")
+			//	serialized.WriteString(   strconv.FormatInt(int64(x.(byte)), 10)   )
+			//	serialized.WriteString(";")
 			//case rune:
-			//	serialized = "i:"+ strconv.FormatInt(int64(x.(rune)), 10) +";"
+			//	serialized.WriteString("i:")
+			//	serialized.WriteString(   strconv.FormatInt(int64(x.(rune)), 10)   )
+			//	serialized.WriteString(";")
 
 			case uint:
-				serialized = "i:"+ strconv.FormatUint(uint64(x.(uint)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatUint(uint64(x.(uint)), 10)   )
+				serialized.WriteString(";")
 			case int:
-				serialized = "i:"+ strconv.FormatInt(int64(x.(int)), 10) +";"
+				serialized.WriteString("i:")
+				serialized.WriteString(   strconv.FormatInt(int64(x.(int)), 10)   )
+				serialized.WriteString(";")
 
 			case string:
-				serialized = "s:"+ strconv.FormatInt(int64(len(x.(string))), 10) +":\""+ x.(string) +"\";"
+				serialized.WriteString("s:")
+				serialized.WriteString(   strconv.FormatInt(int64(len(x.(string))), 10)   )
+				serialized.WriteString(":\"")
+				serialized.WriteString(   x.(string)   )
+				serialized.WriteString("\";")
 
 
 			case []interface{}:
-				serialized = "a:"+ strconv.FormatInt(int64(len(x.([]interface{}))), 10) +":{"
+				serialized.WriteString("a:")
+				serialized.WriteString(   strconv.FormatInt(int64(len(x.([]interface{}))), 10)   )
+				serialized.WriteString(":{")
 				for i := 0; i < len(x.([]interface{})); i++ {
-					serialized += Serialize(i)
-					serialized += Serialize(x.([]interface{})[i])
+					serialized.WriteString(   Serialize(i)   )
+					serialized.WriteString(   Serialize(x.([]interface{})[i])   )
 				}
-				serialized += "}"
+				serialized.WriteString("}")
 
 			case map[string]interface{}:
-				serialized = "a:"+ strconv.FormatInt(int64(len(x.(map[string]interface{}))), 10) +":{"
+				serialized.WriteString("a:")
+				serialized.WriteString(   strconv.FormatInt(int64(len(x.(map[string]interface{}))), 10)   )
+				serialized.WriteString(":{")
 				for key, value := range x.(map[string]interface{}) {
-					serialized += Serialize(key)
-					serialized += Serialize(value)
+					serialized.WriteString(   Serialize(key)   )
+					serialized.WriteString(   Serialize(value)   )
 				}
-				serialized += "}"
+				serialized.WriteString("}")
 
 			default:
-				serialized = ""
+				serialized.WriteString("")
 
 
 //@TODO: Add support for arrays
@@ -81,7 +122,7 @@ func Serialize(x interface{}) string {
 
 
 	// Return.
-		return serialized	
+		return serialized.String()
 }
 
 func serializeFloat32(x float32) string {
