70f9177d836af64225f81e05469eb70ac8e66986
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-11-20 22:04:28 -0800

    changed the formating of the code

--- php_serialize_test.go
+++ php_serialize_test.go
@@ -1,12 +1,8 @@
 package php
 
-
-
 import (
 	"testing"
 )
-
-
 
 func TestSerializeString(t *testing.T) {
 
@@ -15,57 +11,48 @@ func TestSerializeString(t *testing.T) {
 
 	var x string
 
-
-
 	x = "apple"
 	expected = "s:5:\"apple\";"
 
-	if serialized = Serialize(x); expected != serialized  {
+	if serialized = Serialize(x); expected != serialized {
 		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v]. Instead got [%#v].", x, expected, serialized)
 	}
-
 
 	x = "123"
 	expected = "s:3:\"123\";"
 
-	if serialized = Serialize(x); expected != serialized  {
+	if serialized = Serialize(x); expected != serialized {
 		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v]. Instead got [%#v].", x, expected, serialized)
 	}
-
 
 	x = "-3"
 	expected = "s:2:\"-3\";"
 
-	if serialized = Serialize(x); expected != serialized  {
+	if serialized = Serialize(x); expected != serialized {
 		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v]. Instead got [%#v].", x, expected, serialized)
 	}
-
 
 	x = "12.34"
 	expected = "s:5:\"12.34\";"
 
-	if serialized = Serialize(x); expected != serialized  {
+	if serialized = Serialize(x); expected != serialized {
 		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v]. Instead got [%#v].", x, expected, serialized)
 	}
-
 
 	x = "-12.34"
 	expected = "s:6:\"-12.34\";"
 
-	if serialized = Serialize(x); expected != serialized  {
+	if serialized = Serialize(x); expected != serialized {
 		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v]. Instead got [%#v].", x, expected, serialized)
 	}
-
 
 	x = "apple \"ok\" banana"
 	expected = "s:17:\"apple \"ok\" banana\";"
 
-	if serialized = Serialize(x); expected != serialized  {
+	if serialized = Serialize(x); expected != serialized {
 		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v]. Instead got [%#v].", x, expected, serialized)
 	}
 }
-
-
 
 func TestSerializeMapWithStringKeys(t *testing.T) {
 
@@ -74,9 +61,7 @@ func TestSerializeMapWithStringKeys(t *testing.T) {
 
 	var x map[string]interface{}
 
-
-
-	x = map[string]interface{}{"one":"a", "two":"bb", "three":"ccc"}
+	x = map[string]interface{}{"one": "a", "two": "bb", "three": "ccc"}
 	expected1 = "a:3:{s:3:\"one\";s:1:\"a\";s:3:\"two\";s:2:\"bb\";s:5:\"three\";s:3:\"ccc\";}"
 	expected2 = "a:3:{s:3:\"one\";s:1:\"a\";s:5:\"three\";s:3:\"ccc\";s:3:\"two\";s:2:\"bb\";}"
 	expected3 = "a:3:{s:3:\"two\";s:2:\"bb\";s:3:\"one\";s:1:\"a\";s:5:\"three\";s:3:\"ccc\";}"
@@ -84,22 +69,18 @@ func TestSerializeMapWithStringKeys(t *testing.T) {
 	expected5 = "a:3:{s:3:\"two\";s:2:\"bb\";s:5:\"three\";s:3:\"ccc\";s:3:\"one\";s:1:\"a\";}"
 	expected6 = "a:3:{s:5:\"three\";s:3:\"ccc\";s:3:\"two\";s:2:\"bb\";s:3:\"one\";s:1:\"a\";}"
 
-	if serialized = Serialize(x); expected1 != serialized && expected2 != serialized && expected3 != serialized && expected4 != serialized && expected5 != serialized && expected6 != serialized  {
+	if serialized = Serialize(x); expected1 != serialized && expected2 != serialized && expected3 != serialized && expected4 != serialized && expected5 != serialized && expected6 != serialized {
 		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v] | [%#v] | [%#v] | [%#v] | [%#v] | [%#v]. Instead got [%#v].", x, expected1, expected2, expected3, expected4, expected5, expected6, serialized)
 	}
 
-
-	x = map[string]interface{}{"apple":"banana", "cherry":"watermelon"}
+	x = map[string]interface{}{"apple": "banana", "cherry": "watermelon"}
 	expected1 = "a:2:{s:5:\"apple\";s:6:\"banana\";s:6:\"cherry\";s:10:\"watermelon\";}"
 	expected2 = "a:2:{s:6:\"cherry\";s:10:\"watermelon\";s:5:\"apple\";s:6:\"banana\";}"
-
 
 	if serialized = Serialize(x); expected1 != serialized && expected2 != serialized {
 		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v] | [%#v]. Instead got [%#v].", x, expected1, expected2, serialized)
 	}
 }
-
-
 
 func TestSerializeArray(t *testing.T) {
 
@@ -108,13 +89,11 @@ func TestSerializeArray(t *testing.T) {
 
 	var x [3]string
 
-
-
 	x = [3]string{"a", "bb", "ccc"}
 	expected = "a:3:{i:0;s:1:\"a\";i:1;s:2:\"bb\";i:2;s:3:\"ccc\";}"
 
-	if serialized = Serialize(x); expected != serialized  {
-//@TODO
-//		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v]. Instead got [%#v].", x, expected, serialized)
+	if serialized = Serialize(x); expected != serialized {
+		//@TODO
+		//		t.Errorf("Serialize() did not serialize [%#v] as expected [%#v]. Instead got [%#v].", x, expected, serialized)
 	}
 }
